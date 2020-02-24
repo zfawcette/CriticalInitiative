c54952819d08c17fd3c17fa377fa5e6057f7337d
bew61990 <bew61990@ucmo.edu>
2020-02-24 11:48:42 -0600

    start of update to the Forgot password and Create Account pages. Also testing the use of git

--- src/components/CreateAccount/CreateAccount.js
+++ src/components/CreateAccount/CreateAccount.js
@@ -5,6 +5,18 @@ function CreateAccount() {
   return (
     <div>
           <h1>CreateAccount</h1>
+
+          
+          Username:<input type="text" id="Username" />
+          <br /><br />
+          Email Address:<input type="text" id="EmailAddress" />
+          <br /><br />
+          Password:<input type="password" id="Password" />
+          <br /><br />
+          Confirm Password:<input type="password" id="ConfirmPassword" />
+          <br /><br />
+          <button onClick={event => window.location.href = '/'}> Submit</button>
+              
     </div>
   );
 }
--- src/components/ForgotPassword/ForgotPassword.js
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -4,7 +4,15 @@ import './ForgotPassword.css';
 function ForgotPassword() {
   return (
     <div>
-          <h1>ForgotPassword</h1>
+          <h1>Forgot Password</h1>
+          Enter your username, and we will send you a link
+          to reset your password to the associcated email
+          <br/>
+          <br/>
+          Username:<input type="text" id="Username"/>
+          <br/>
+          <button onClick={event => window.location.href = '/'}> Submit</button>     
+
     </div>
   );
 }
