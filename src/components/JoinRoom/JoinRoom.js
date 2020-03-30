979ccfb457b61fed901e54062939b0cfc8b62342
Stephen-Elmore <60947796+Stephen-Elmore@users.noreply.github.com>
2020-03-30 15:17:46 -0500

    JoinRoom Framework
    Updated Join Room with basic framework scripts

--- src/components/JoinRoom/JoinRoom.js
+++ src/components/JoinRoom/JoinRoom.js
@@ -5,6 +5,14 @@ function JoinRoom() {
   return (
     <div>
           <h1>JoinRoom</h1>
+          <h3>Host Room</h3>
+          <button onClick={console.log("Room Created")}>Host Room</button>
+          <h3>Join Room</h3>
+          <input
+               type='text'
+          />
+          <button onClick={console.log("Joining Room")}>Join Room</button>
+
     </div>
   );
 }
